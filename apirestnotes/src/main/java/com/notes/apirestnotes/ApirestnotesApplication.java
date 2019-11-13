b1afb7a38cb08b23f98dc77194acd1b379b4468c
Raphael Americano <raphaelamericano@MacBook-Pro-de-Claudia.local>
2019-11-13 15:55:30 -0300

    Adicionando o setup do Spring

---
+++ apirestnotes/src/main/java/com/notes/apirestnotes/ApirestnotesApplication.java
@@ -0,0 +1,13 @@
+package com.notes.apirestnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestnotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestnotesApplication.class, args);
+	}
+
+}
---
+++ apirestnotes/src/test/java/com/notes/apirestnotes/ApirestnotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.notes.apirestnotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestnotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
